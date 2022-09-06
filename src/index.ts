ca05ddd832a11eb1cef056e582533a32eac5bd83
Jennifer Moore <jenniferplusplus@gmail.com>
2022-09-06 00:44:07 -0500

    tests not working, but getting close try ioredis-mock defineGlobalFunction

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,1 @@
-import { Instrumentation } from './instrumentation';
-
 export * from './instrumentation';
-export { Instrumentation as BullMqInstrumentation };
---
+++ test/instrumentation.test.ts
@@ -0,0 +1,62 @@
+import rewiremock from 'rewiremock';
+rewiremock.overrideEntryPoint(module);
+
+import * as assert from 'assert';
+const Redis = require('ioredis-mock');
+rewiremock('ioredis').with(Redis);
+rewiremock.enable();
+
+import { context, trace } from '@opentelemetry/api';
+import { SemanticAttributes } from '@opentelemetry/semantic-conventions';
+import { AsyncHooksContextManager } from '@opentelemetry/context-async-hooks';
+import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node';
+import {
+  InMemorySpanExporter,
+  SimpleSpanProcessor,
+} from '@opentelemetry/sdk-trace-base';
+import {Queue} from 'bullmq';
+
+rewiremock.disable();
+
+import {Instrumentation} from '../src'
+
+
+const instrumentation = new Instrumentation();
+const contextManager = new AsyncHooksContextManager().enable();
+const memoryExporter = new InMemorySpanExporter();
+const provider = new NodeTracerProvider();
+const spanProcessor = new SimpleSpanProcessor(memoryExporter);
+instrumentation.setTracerProvider(provider);
+context.setGlobalContextManager(contextManager);
+
+const tracer = provider.getTracer('default');
+
+provider.addSpanProcessor(spanProcessor);
+instrumentation.enable();
+
+
+describe('bullmq', () => {
+  const connection = {host: 'localhost'};
+
+  beforeEach(() => {
+    instrumentation.enable();
+    // connection = new Redis(QueueOpts);
+  });
+
+  afterEach(() => {
+    contextManager.disable();
+    contextManager.enable();
+    memoryExporter.reset();
+    instrumentation.disable();
+  });
+
+  describe('Queue', () => {
+    it('should not generate any spans when disabled', async () => {
+      const q = new Queue('test', {connection});
+      await q.add('job', {test: 'yes'});
+
+      const spans = memoryExporter.getFinishedSpans();
+      assert.strictEqual(spans.length, 0);
+    });
+  });
+});
